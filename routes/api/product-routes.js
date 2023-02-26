642d8b3c676fbd3de1a26465d78b7d67fae3af1b
jessikea <jessikea@jtran.git>
2023-02-26 15:25:32 -0500

    routes added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,11 +6,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({ 
+    include: [
+      Category, 
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((productData) => res.json(productData))
+    .catch((err) => { res.status(500).json(err); });
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: { id: req.params.id },
+    include: [
+      Category, 
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+  .then((products)=> res.json(products))
+  .catch((err) => { res.status(500).json(err); });
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
