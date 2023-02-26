4e0e37fad37bafb19e02a3f7327a625f57ffcbee
jessikea <jessikea@jtran.git>
2023-02-26 14:46:31 -0500

    routes added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,56 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ],
+    // be sure to include its associated Product data
+  })
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => { res.status(500).json(err) });
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
+  router.get('/:id', (req, res) => {
+    // find a single tag by its `id`
+    Tag.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: Product,
+          through: ProductTag
+        }
+      ],
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
+      // be sure to include its associated Product data
+    });
+  });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+  router.post('/', (req, res) => {
+    Tag.create(req.body)
+      .then((tag) => res.status(200).json(tag))
+      .catch((err) => { res.status(400).json(err) });
+  });
 
-module.exports = router;
+
+  router.put('/:id', (req, res) => {
+    // update a tag's name by its `id` value
+    Tag.update(req.body, {
+      where: { id: req.params.id },
+    })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => { res.status(400).json(err) });
+  });
+
+  router.delete('/:id', (req, res) => {
+    // delete on tag by its `id` value
+    Tag.destroy({ where: { id: req.params.id } })
+      .then((tag) => res.status(200).json(tag))
+      .catch((err) => { res.status(400).json(err) });
+  });
+
+  module.exports = router;
